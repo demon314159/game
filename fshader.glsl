ac479cc9d21b9c583edc191940ffa4e0d95f7dd2
Andy Summers <demon314159@gmail.com>
2021-05-26 23:23:28 -0400

    Draw green triangles on red background

--- fshader.glsl
+++ fshader.glsl
@@ -12,7 +12,7 @@ varying vec2 v_texcoord;
 void main()
 {
     // Set fragment color from texture
-    gl_FragColor = texture2D(texture, v_texcoord);
+    gl_FragColor = vec4(0,1,0,1);
 }
 //! [0]
 

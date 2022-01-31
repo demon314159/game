018192d182033c7921633a792e86f52272d341e5
Andy Summers <demon314159@gmail.com>
2022-01-31 00:30:36 -0500

    Optimize shader and add new animation id for morph button

--- vshader.glsl
+++ vshader.glsl
@@ -7,6 +7,7 @@ precision mediump float;
 uniform mat4 mvp_matrix;
 uniform mat4 rot_matrix;
 uniform mat4 marker_matrix;
+uniform mat4 morph_matrix;
 
 attribute vec3 a_position;
 attribute vec3 a_normal;
@@ -22,17 +23,25 @@ void main()
     billy = normalize(a_normal);
 
 // Calculate vertex position in screen space
-    if (a_animation_id == 2.0) {
-        gl_Position = mvp_matrix *  marker_matrix * vec4(a_position, 1.0);
-    } else {
+    if (a_animation_id == 0.0) { // Optimize this most common case
         gl_Position = mvp_matrix * vec4(a_position, 1.0);
-    }
-    if (a_animation_id == 1.0) {
-        v_normal = vec4(0.0, 0.0, 1.0, 0.0);
-    } else {
         v_normal = rot_matrix * vec4(billy, 0.0);
+    } else {
+        if (a_animation_id == 3.0) {
+            gl_Position = morph_matrix * vec4(a_position, 1.0);
+        } else if (a_animation_id == 2.0) {
+            gl_Position = mvp_matrix *  marker_matrix * vec4(a_position, 1.0);
+        } else { // Treat unknowns like 0.0
+            gl_Position = mvp_matrix * vec4(a_position, 1.0);
+        }
+        if (a_animation_id == 3.0) {
+            v_normal = vec4(billy, 0.0);
+        } else if (a_animation_id == 1.0) {
+            v_normal = vec4(0.0, 0.0, 1.0, 0.0);
+        } else { // Treat unknowns like 0.0
+            v_normal = rot_matrix * vec4(billy, 0.0);
+        }
     }
-
     // Pass color coordinate to fragment shader
     v_color = a_color;
 }

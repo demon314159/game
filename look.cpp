596f71173902ccf43f4793c4fa038cf8aca23a02
Andy Summers <demon314159@gmail.com>
2023-04-24 00:34:19 -0400

    Remove unused codep

--- look.cpp
+++ look.cpp
@@ -1,7 +1,5 @@
 
 #include "look.h"
-#include <string.h>
-#include <stdio.h>
 
 float Look::dimx = 1.0;
 float Look::dimh = 1.0;
@@ -9,8 +7,6 @@ float Look::dimb = 0.025;
 float Look::dimt = 0.05;
 
 bool Look::m_3d = false;
-QString Look::m_startup_name(".session.brk");
-QString Look::m_session_name(".session.brk");
 
 void Look::process_options(int argv, char** args)
 {
@@ -19,28 +15,6 @@ void Look::process_options(int argv, char** args)
             set_3d(true);
         }
     }
-    for (int i = 1; i < argv; i++) {
-        QString s(args[i]);
-        if (!s.contains('-')) {
-            if (s.contains('.')) {
-                m_startup_name = s;
-            } else {
-                m_startup_name = s + QString(".brk");
-            }
-            printf("file name = '%s'\n", m_startup_name.toLatin1().data());
-            return;
-        }
-    }
-}
-
-QString Look::startup_name()
-{
-    return m_startup_name;
-}
-
-QString Look::session_name()
-{
-    return m_session_name;
 }
 
 void Look::set_3d(bool v)
@@ -61,14 +35,6 @@ PaintCan Look::table_paint()
         return PaintCan(0.4, 0.8, 1.0);
 }
 
-PaintCan Look::marker_paint()
-{
-    if (Look::get_3d())
-        return PaintCan(0.6, 0.6, 0.6);
-    else
-        return PaintCan(0.0, 1.8, 0.0);
-}
-
 PaintCan Look::grid_paint()
 {
     return PaintCan(0.5, 0.5, 0.5);
@@ -82,72 +48,3 @@ PaintCan Look::brick_paint()
         return PaintCan(1.0, 0.0, 0.0);
 }
 
-PaintCan Look::ledge_paint()
-{
-    return PaintCan(0.8, 0.8, 0.8);
-}
-
-PaintCan Look::frame_paint()
-{
-    return PaintCan(1.0, 1.0, 1.0);
-}
-
-PaintCan Look::window_paint()
-{
-    return PaintCan(1.0, 1.0, 1.0);
-}
-
-PaintCan Look::door_paint()
-{
-    return PaintCan(1.0, 1.0, 1.0);
-}
-
-PaintCan Look::roof_paint()
-{
-    return PaintCan(0.0, 0.8, 0.2);
-}
-
-PaintCan Look::vbutton_paint()
-{
-    if (Look::get_3d())
-        return PaintCan(0.0, 0.0, 0.0);
-    else
-        return PaintCan(0.0, 0.0, 1.0);
-}
-
-PaintCan Look::cancel_paint()
-{
-    if (Look::get_3d())
-        return PaintCan(0.0, 0.0, 0.0);
-    else
-        return PaintCan(1.0, 0.0, 0.0);
-}
-
-PaintCan Look::done_paint()
-{
-    if (Look::get_3d())
-        return PaintCan(0.0, 0.0, 0.0);
-    else
-        return PaintCan(0.0, 0.7, 0.3);
-}
-
-PaintCan Look::vbackground_paint()
-{
-    if (Look::get_3d())
-        return PaintCan(0.5, 0.5, 0.5);
-    else
-        return PaintCan(1.0, 192.0 / 256.0, 203.0 / 256.0); // pink
-}
-
-PaintCan Look::glass_paint()
-{
-    if (Look::get_3d())
-        return PaintCan(0.96, 0.96, 0.96);
-    else
-        return PaintCan(0.96, 0.99, 1.0);
-}
-
-float Look::glass_animation_id()
-{
-    return 99.0;
-}
--- look.h
+++ look.h
@@ -3,7 +3,6 @@
 #define _LOOK_H_
 
 #include "paint_can.h"
-#include <QString>
 
 class Look
 {
@@ -14,29 +13,11 @@ public:
     static float dimb;
     static float dimt;
     static PaintCan table_paint();
-    static PaintCan marker_paint();
     static PaintCan grid_paint();
     static PaintCan brick_paint();
-    static PaintCan ledge_paint();
-    static PaintCan frame_paint();
-    static PaintCan window_paint();
-    static PaintCan door_paint();
-    static PaintCan roof_paint();
-    static PaintCan vbutton_paint();
-    static PaintCan cancel_paint();
-    static PaintCan done_paint();
-    static PaintCan vbackground_paint();
-    static PaintCan glass_paint();
-    static float glass_animation_id();
-    static void set_3d(bool v);
-    static bool get_3d();
-    static QString startup_name();
-    static QString session_name();
 
 private:
     static bool m_3d;
-    static QString m_startup_name;
-    static QString m_session_name;
 };
 
 #endif // _LOOK_H_
